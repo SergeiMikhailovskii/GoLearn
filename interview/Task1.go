d6c84020272657734dcf8b48e159a3dfd6bcbebb
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2020-12-30 00:57:40 +0300

    Solved task1

--- interview/Task1.go
+++ interview/Task1.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var amount int
+	_, _ = fmt.Scanf("%d", &amount)
+
+	pairs := 0
+	set := make(map[int]bool)
+	for i := 0; i < amount; i++ {
+		var number int
+		_, _ = fmt.Scanf("%d", &number)
+
+		if set[number] {
+			set[number] = false
+			pairs++
+		} else {
+			set[number] = true
+		}
+	}
+	fmt.Println(pairs)
 }
