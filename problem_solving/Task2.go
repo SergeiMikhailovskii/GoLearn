a7a5c49abbcca3ab8cf4c9826b30a23c05780bbd
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2020-12-30 23:49:59 +0300

    Solved Task2 problem_solving

--- problem_solving/Task2.go
+++ problem_solving/Task2.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var amount int
+	_, _ = fmt.Scanf("%d", &amount)
+
+	sum := 0
+
+	for i := 0; i < amount; i++ {
+		value := 0
+		_, _ = fmt.Scanf("%d", &value)
+		sum += value
+	}
+
+	fmt.Println(sum)
 }
