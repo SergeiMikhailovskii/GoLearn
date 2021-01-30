6e1cccf07cf64883ccf2fc7361b202b4c36fd223
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2021-01-30 22:18:26 +0300

    Solved Day29 30_days_of_coding

---
+++ 30_days_of_coding/Day29.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nk := strings.Split(readLine(reader), " ")
+
+		nTemp, err := strconv.ParseInt(nk[0], 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		kTemp, err := strconv.ParseInt(nk[1], 10, 64)
+		checkError(err)
+		k := int32(kTemp)
+
+		max := math.MinInt32
+		for i := 1; i < int(n); i++ {
+			for j := i + 1; j <= int(n); j++ {
+				bitwiseAnd := i & j
+				if bitwiseAnd < int(k) {
+					if max < bitwiseAnd {
+						max = bitwiseAnd
+					}
+				}
+			}
+		}
+
+		fmt.Println(max)
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
